65a5d982671e38b8d3c08624e4a0773ecd07d72a
Alex  Rutan <alexrutan04@gmail.com>
2021-07-14 13:29:00 -0700

    first two app rputes and templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, render_template
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -14,9 +14,25 @@ from flask_debugtoolbar import DebugToolbarExtension
 app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
-
-
-
-
-
 db.create_all()
+
+
+@app.route('/')
+def load_homepage():
+    """Loads our homepage"""
+
+    """TODO: need to change to redirect"""
+    return render_template(
+        "users.html"
+    )
+
+
+
+
+@app.route('/users/new')
+def load_create_user_form():
+    """Loads create user page and our create user form"""
+
+    return render_template(
+        
+    )
