d145c232847e0754cef4d6ea5381c75a04403743
Wukongthethird <simonzhang9518@gmail.com>
2021-07-15 14:17:26 -0700

    tests

--- sample_model.py
+++ sample_model.py
@@ -5,14 +5,17 @@ from app import app
 db.drop_all()
 db.create_all()    
 
+def initial_information():
+    User.query.delete()
 
-alex_rutan = User( first_name = 'alex' , last_name =  'rutan')
-simon_zhang = User( first_name = 'simon' , last_name =  'zhang')
-joel_burton = User( first_name = 'joel' , last_name =  'burtan')
-karmen_ming = User( first_name = 'karmen' , last_name =  'ming')
-test_users = [ alex_rutan, simon_zhang , joel_burton, karmen_ming]
+    alex_rutan = User( first_name = 'alex' , last_name =  'rutan')
+    simon_zhang = User( first_name = 'simon' , last_name =  'zhang')
+    joel_burton = User( first_name = 'joel' , last_name =  'burtan')
+    karmen_ming = User( first_name = 'karmen' , last_name =  'ming')
+    
+    test_users = [ alex_rutan, simon_zhang , joel_burton, karmen_ming]
+    db.session.add_all(test_users)
+    db.session.commit()
 
-
-db.session.add_all(test_users)
-db.session.commit()
+initial_information()
 
--- test_app.py
+++ test_app.py
@@ -1,14 +1,20 @@
 from unittest import TestCase
 from app import app
-from models import User
-from flask_sqlalchemy import SQLAlchemy
+from models import User, db 
+from sample_model import initial_information
 
-db = SQLAlchemy()
 
 app.config['TESTING'] = True
 
 # This is a bit of hack, but don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test_blogly'
+app.config['SQLALCHEMY_ECHO'] = False
+
+
+db.drop_all()
+db.create_all()
 
 
 class SampleAppTestCase(TestCase):
@@ -18,53 +24,61 @@ class SampleAppTestCase(TestCase):
         """Stuff to do before every test."""
 
         self.client = app.test_client()
-        app.config['TESTING'] = True
+        initial_information()
+        self.user_id = User.query.get(1).id
+        print( 'LOOK AT HERE' , self)
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+
+    # def test_homepage(self):
+    #     """Make sure information is in the session and HTML is displayed"""
+
+    #     with self.client as client:
+    #         response = client.get('/')
+
+    #         # testing for routing to root
+    #         self.assertEqual(response.status_code, 302)
 
 
-    def test_homepage(self):
-        """Make sure information is in the session and HTML is displayed"""
+    # def test_homepage_redirect(self):
+    #     with self.client as client:
+    #         response = client.get('/users')
+    #         html = response.get_data(as_text=True)
 
-        with self.client as client:
-            response = client.get('/')
-
-            # testing for routing to root
-            self.assertEqual(response.status_code, 302)
-
-
-    def test_homepage_redirect(self):
-        with self.client as client:
-            response = client.get('/users')
-            html = response.get_data(as_text=True)
-
-            # Testing template #
-            self.assertEqual(response.status_code, 200)
-            # self.assertIn('ming', html)
-            self.assertIn('testing', html)
+    #         # Testing template #
+    #         self.assertEqual(response.status_code, 200)
+    #         # self.assertIn('ming', html)
+    #         self.assertIn('testing', html)
 
 
-    def test_create_user(self):
-        with self.client as client:
-            response = client.post('/users/new', 
-                                    data={'first_name': 'alextest', 'last_name': 'rutantest', 'image_url': None})
-            html = response.get_data(as_text=True)
+    # def test_create_user(self):
+    #     with self.client as client:
+    #         response = client.post('/users/new', 
+    #                                 data={'first_name': 'alextest', 'last_name': 'rutantest', 'image_url': 
+    #                                 "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/440px-SpongeBob_SquarePants_character.svg.png"},follow_redirects=True)
+    #         html = response.get_data(as_text=True)
 
-            new_user =  User(first_name='alextest', last_name='rutantest',image_url=None)
-            db.session.add(new_user)
-            db.session.commit()
+    #         new_user =  User(first_name='alextest', last_name='rutantest',image_url=None)
+    #         db.session.add(new_user)
+    #         db.session.commit()
 
-            self.assertEqual(response.status_code, 302)
-            self.assertIn('testing', html)
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertIn('testing', html)
+
 
     def test_edit_user(self):
         with self.client as client:
-            response = client.post('/users/3/edit', 
-                                    data={'first_name': 'alextest', 'last_name': '', 'image_url': None})
+            response = client.post(f'/users/{self.user_id}/edit', 
+                                    data={'first_name': 'alextest', 'last_name': '', 'image_url': "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/440px-SpongeBob_SquarePants_character.svg.png"},follow_redirects=True)
             html = response.get_data(as_text=True)
 
             db.session.commit()
 
-            self.assertEqual(response.status_code, 302)
-            self.assertIn('testing', html)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('alextest', html)
 
 
     # MAKE A TEST THAT TESTS IF A 404 STATUS CODE is  AN ID THAT DOESNT EXIST IS PASSED IN 
